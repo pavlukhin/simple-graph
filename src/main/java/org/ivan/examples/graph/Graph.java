d257648988a6c2f1645a0267e0d8857d2be2ade0
pavlukhin <vololo100@gmail.com>
2020-05-22 17:29:01 +0300

    Naive Graph.getPath method and tests.

--- src/main/java/org/ivan/examples/graph/Graph.java
+++ src/main/java/org/ivan/examples/graph/Graph.java
@@ -1,17 +1,35 @@
 package org.ivan.examples.graph;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Graph {
-    private static class DirectedEdge {
+    public interface Edge {
+        Vertex from();
+        Vertex to();
+    }
+
+    private static class DirectedEdge implements Edge {
         private final Vertex from;
         private final Vertex to;
 
         private DirectedEdge(Vertex from, Vertex to) {
             this.from = Objects.requireNonNull(from);
             this.to = Objects.requireNonNull(to);
+        }
+
+        @Override public Vertex from() {
+            return from;
+        }
+
+        @Override public Vertex to() {
+            return to;
         }
 
         @Override
@@ -57,5 +75,54 @@ public class Graph {
         if (!edges.add(edge)) {
             throw new IllegalArgumentException("Edge is already in graph: " + v1 + " -> " + v2);
         }
+    }
+
+    // t0d0 consider something custom instead of standard Java Optional
+    public Optional<List<Edge>> getPath(Vertex from, Vertex to) {
+        Objects.requireNonNull(from);
+        Objects.requireNonNull(to);
+        if (!vertices.contains(from)) {
+            throw new IllegalArgumentException("Vertex does not belong to graph: " + from);
+        }
+        if (!vertices.contains(to)) {
+            throw new IllegalArgumentException("Vertex does not belong to graph: " + to);
+        }
+
+        if (from.equals(to)) {
+            return Optional.of(Collections.emptyList());
+        }
+        // t0d0 Let's do better
+        LinkedList<Edge> path = new LinkedList<>();
+        Set<Vertex> visitedVertices = new HashSet<>();
+        if (advancePath(from, to, path, visitedVertices)) {
+            return Optional.of(path);
+        }
+        return Optional.empty();
+    }
+
+    private boolean advancePath(Vertex from, Vertex to, LinkedList<Edge> path, Set<Vertex> visitedVertices) {
+        visitedVertices.add(from);
+        for (Vertex v : getConnectedVertices(from)) {
+            path.add(new DirectedEdge(from, v));
+            if (v.equals(to)) {
+                return true;
+            }
+            if (!visitedVertices.contains(v) && advancePath(v, to, path, visitedVertices)) {
+                return true;
+            }
+            path.removeLast();
+        }
+        return false;
+    }
+
+    private List<Vertex> getConnectedVertices(Vertex from) {
+        // t0d0 Let's do better
+        ArrayList<Vertex> vertices = new ArrayList<>();
+        for (DirectedEdge edge : edges) {
+            if (edge.from.equals(from)) {
+                vertices.add(edge.to);
+            }
+        }
+        return vertices;
     }
 }
--- src/test/java/org/ivan/examples/graph/GraphPathTest.java
+++ src/test/java/org/ivan/examples/graph/GraphPathTest.java
@@ -1,10 +1,118 @@
 package org.ivan.examples.graph;
 
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GraphPathTest {
     @Test
-    void getIdentityPath() {
+    void nullVertexPath() {
         Graph g = new Graph();
+
+        assertThrows(NullPointerException.class, () -> g.getPath(null, new Vertex()));
+        assertThrows(NullPointerException.class, () -> g.getPath(new Vertex(), null));
+    }
+
+    @Test
+    void alienVertexPath() {
+        Graph g = new Graph();
+        Vertex v = new Vertex();
+        g.addVertex(v);
+
+        assertThrows(IllegalArgumentException.class, () -> g.getPath(v, new Vertex()));
+        assertThrows(IllegalArgumentException.class, () -> g.getPath(new Vertex(), v));
+    }
+
+    @Test
+    void disjointVerticesPath() {
+        Graph g = new Graph();
+        Vertex v1 = new Vertex();
+        Vertex v2 = new Vertex();
+        g.addVertex(v1);
+        g.addVertex(v2);
+
+        Optional<List<Graph.Edge>> opath = g.getPath(v1, v2);
+
+        assertFalse(opath.isPresent());
+    }
+
+    @Test
+    void identityPath() {
+        Graph g = new Graph();
+        Vertex v = new Vertex();
+        g.addVertex(v);
+
+        Optional<List<Graph.Edge>> opath = g.getPath(v, v);
+
+        assertTrue(opath.isPresent());
+        assertTrue(opath.get().isEmpty());
+    }
+
+    @Test
+    void oneEdgePath() {
+        Graph g = new Graph();
+        Vertex v1 = new Vertex();
+        Vertex v2 = new Vertex();
+        g.addVertex(v1);
+        g.addVertex(v2);
+        g.addEdge(v1, v2);
+
+        Optional<List<Graph.Edge>> opath1 = g.getPath(v1, v2);
+        assertTrue(opath1.isPresent());
+        assertEquals(1, opath1.get().size());
+        Graph.Edge edge = opath1.get().get(0);
+        assertEquals(v1, edge.from());
+        assertEquals(v2, edge.to());
+
+        assertFalse(g.getPath(v2, v1).isPresent());
+    }
+
+    @Test
+    void twoEdgePath() {
+        Graph g = new Graph();
+        Vertex v1 = new Vertex();
+        Vertex v2 = new Vertex();
+        Vertex v3 = new Vertex();
+        g.addVertex(v1);
+        g.addVertex(v2);
+        g.addVertex(v3);
+        g.addEdge(v1, v2);
+        g.addEdge(v2, v3);
+
+        Optional<List<Graph.Edge>> opath = g.getPath(v1, v3);
+        assertTrue(opath.isPresent());
+        List<Graph.Edge> path = opath.get();
+        assertEquals(2, path.size());
+        Graph.Edge e0 = path.get(0);
+        Graph.Edge e1 = path.get(1);
+        assertEquals(v1, e0.from());
+        assertEquals(v2, e0.to());
+        assertEquals(v2, e1.from());
+        assertEquals(v3, e1.to());
+    }
+
+    @Test
+    void noPathAndCycle() {
+        Graph g = new Graph();
+
+        Vertex v1 = new Vertex();
+        Vertex v2 = new Vertex();
+        Vertex v3 = new Vertex();
+        Vertex v4 = new Vertex();
+        g.addVertex(v1);
+        g.addVertex(v2);
+        g.addVertex(v3);
+        g.addVertex(v4);
+        g.addEdge(v1, v2);
+        g.addEdge(v2, v1);
+        g.addEdge(v3, v4);
+        g.addEdge(v4, v3);
+
+        assertFalse(g.getPath(v1, v3).isPresent());
     }
 }
