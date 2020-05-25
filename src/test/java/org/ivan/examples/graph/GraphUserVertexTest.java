243f987204a6932f674627c543e0bae6509dc7bd
pavlukhin <vololo100@gmail.com>
2020-05-25 10:35:01 +0300

    Add test checking that Graph uses provided vertex "equals" method.

---
+++ src/test/java/org/ivan/examples/graph/GraphUserVertexTest.java
@@ -0,0 +1,23 @@
+package org.ivan.examples.graph;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GraphUserVertexTest {
+    @Test
+    void graphUsesOverriddenVertexEquals() {
+        Graph<String> g = Graph.newDirectedGraph();
+        g.addVertex("a");
+        g.addVertex("b");
+        g.addEdge("a", "b");
+
+        //noinspection OptionalGetWithoutIsPresent
+        List<Graph.Edge<String>> path = g.getPath("a", "b").get();
+
+        assertEquals(1, path.size());
+        assertEquals("a", path.get(0).from());
+        assertEquals("b", path.get(0).to());
+    }
+}
